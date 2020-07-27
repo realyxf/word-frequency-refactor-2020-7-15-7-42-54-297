988a48f036ea9d0843ae3e24b3152a8a16bf411b
realyxf <997915421@qq.com>
2020-07-27 15:22:13 +0800

    [Word Frequency] [Fennie] refactor: drop the useless function and use stream() to join the string.

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,58 +6,37 @@ import java.util.StringJoiner;
 import java.io.CharArrayWriter;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
 
     public String getResult(String inputStr) {
 
-        try {
 
-            String[] words = inputStr.split("\\s+");
+        String[] words = inputStr.split("\\s+");
 
-            //List<Input> inputList = new ArrayList<>();
+        //List<Input> inputList = new ArrayList<>();
 
-            HashMap<String, Integer> inputMap = new HashMap<>();
-            for (String word : words) {
-                if (inputMap.containsKey(word)) {
-                    inputMap.put(word, inputMap.get(word) + 1);
-                }
-                else{
-                    inputMap.put(word, 1);
-                }
-            }
-
-            List<Input> list = new ArrayList<>();
-            for (Map.Entry<String, Integer> entry : inputMap.entrySet()) {
-                Input input = new Input(entry.getKey(), entry.getValue());
-                list.add(input);
-            }
-
-            list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-            StringJoiner joiner = new StringJoiner("\n");
-            for (Input w : list) {
-                String s = w.getValue() + " " + w.getWordCount();
-                joiner.add(s);
-            }
-            return joiner.toString();
-        } catch (Exception e) {
-            return "Calculate Error";
-        }
-    }
-
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
-
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
+        HashMap<String, Integer> inputMap = new HashMap<>();
+        for (String word : words) {
+            if (inputMap.containsKey(word)) {
+                inputMap.put(word, inputMap.get(word) + 1);
             } else {
-                map.get(input.getValue()).add(input);
+                inputMap.put(word, 1);
             }
         }
-        return map;
+
+        List<Input> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : inputMap.entrySet()) {
+            Input input = new Input(entry.getKey(), entry.getValue());
+            list.add(input);
+        }
+
+        list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+
+        return list.stream()
+                .map(input -> String.format("%s %s", input.getValue(), input.getWordCount()))
+                .collect(Collectors.joining("\n"));
+
     }
 }
